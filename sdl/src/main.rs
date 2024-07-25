208a0aaa3ace24849d7ced79f78600cde9ac2827
Mark Kimsal <metrofindings@gmail.com>
2024-07-24 23:01:12 -0400

    fix: blending of UI layer

--- sdl/src/main.rs
+++ sdl/src/main.rs
@@ -553,10 +553,10 @@ unsafe fn draw_ui(renderer: &mut Canvas<Window>, tex2: &mut Texture, is_recordin
         sdl2::sys::SDL_BlendMode::SDL_BLENDMODE_NONE,
     );
     sdl2::sys::SDL_SetRenderDrawColor(renderer.raw(), 0, 0, 0, 0);
-    // sdl2::sys::SDL_RenderFillRect(renderer.raw(), std::ptr::null());
+    sdl2::sys::SDL_SetTextureBlendMode(tex2.raw(), sdl2::sys::SDL_BlendMode::SDL_BLENDMODE_NONE);
     sdl2::sys::SDL_RenderClear(renderer.raw());
-
     sdl2::sys::SDL_SetTextureBlendMode(tex2.raw(), sdl2::sys::SDL_BlendMode::SDL_BLENDMODE_BLEND);
+
 
     // sdl2::sys::SDL_RenderClear(renderer.raw());
     if !is_recording {
@@ -565,16 +565,17 @@ unsafe fn draw_ui(renderer: &mut Canvas<Window>, tex2: &mut Texture, is_recordin
     }
 
     let dest_rect = sdl2::sys::SDL_Rect {
-        x: 20,
-        y: 20,
-        w: 60,
-        h: 60,
+        x: 10,
+        y: 10,
+        w: 40,
+        h: 40,
     };
 
+    sdl2::sys::SDL_RenderClear(renderer.raw());
     sdl2::sys::SDL_SetTextureBlendMode(tex2.raw(), sdl2::sys::SDL_BlendMode::SDL_BLENDMODE_BLEND);
     sdl2::sys::SDL_SetTextureAlphaMod(tex2.raw(), 170 as u8);
     sdl2::sys::SDL_SetRenderDrawColor(renderer.raw(), 202, 22, 22, 255);
-    sdl2::sys::SDL_RenderClear(renderer.raw());
+    sdl2::sys::SDL_RenderFillRect(renderer.raw(), std::ptr::addr_of!(dest_rect));
 
     sdl2::sys::SDL_RenderCopy(
         renderer.raw(),
@@ -600,7 +601,8 @@ unsafe fn composite(renderer: &mut Canvas<Window>, tex: &mut Texture, tex2: &mut
     // sdl2::sys::SDL_SetTextureBlendMode(tex.raw(), sdl2::sys::SDL_BlendMode::SDL_BLENDMODE_BLEND);
     // sdl2::sys::SDL_SetTextureAlphaMod(tex.raw(), 25 as u8);
     // sdl2::sys::SDL_SetTextureAlphaMod(tex.raw(), 70 as u8);
-    sdl2::sys::SDL_RenderCopy(renderer.raw(), tex2.raw(), std::ptr::null(), &dest_rect);
+    // sdl2::sys::SDL_RenderCopy(renderer.raw(), tex2.raw(), std::ptr::null(), &dest_rect);
+    sdl2::sys::SDL_RenderCopy(renderer.raw(), tex2.raw(), std::ptr::null(), std::ptr::null());
 
     sdl2::sys::SDL_SetRenderTarget(renderer.raw(), std::ptr::null_mut());
 
